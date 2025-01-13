209b84bf70de777516096cdfab5826546342c374
Gavin Hewitt <ghewitt@email.sc.edu>
2025-01-12 23:29:53 -0500

    still testing workflow scheduling

--- TestBot.py
+++ TestBot.py
@@ -7,10 +7,7 @@ api_url = 'https://api.groupme.com/v3/bots/post'
 
 print('HI! THIS IS RUNNING!')
 
-if dt.now().date() == dt(2025, 1, 12).date():
-    bot_text = 'This is scheduled to run at 7:05. Hopefully it does!'
-else:
-    bot_text = 'Your workflow did not run as intended. Try again :('
+bot_text = f'You scheduled this to run at 11:35. It is currently {dt.now()}.'
 
 
 data = {
